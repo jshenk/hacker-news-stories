564cfbc2df2f0d23e2b33c5328dd4ac788400610
jshenk <jshenk@unleashed-technologies.com>
2020-08-20 09:04:38 -0400

    Updates

---
+++ precache-manifest.12d480f73c9b3e304b87fe9df4c919d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8a2455849223a9cf94a0cf8fc1e57e",
+    "url": "/hacker-news-stories/index.html"
+  },
+  {
+    "revision": "76979799097307fc0177",
+    "url": "/hacker-news-stories/static/css/main.aea449a3.chunk.css"
+  },
+  {
+    "revision": "e5d36a57338c87bbc02a",
+    "url": "/hacker-news-stories/static/js/2.466d9015.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news-stories/static/js/2.466d9015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76979799097307fc0177",
+    "url": "/hacker-news-stories/static/js/main.3a6ed9b3.chunk.js"
+  },
+  {
+    "revision": "8d4c2d96569148c0dfa8",
+    "url": "/hacker-news-stories/static/js/runtime-main.6dfd2121.js"
+  }
+]);
